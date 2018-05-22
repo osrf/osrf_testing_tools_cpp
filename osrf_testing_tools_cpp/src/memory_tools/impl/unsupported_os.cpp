ac281e2815f956d79eedc81d0d24e76b33a4d689
William Woodall <william@osrfoundation.org>
2018-05-21 20:40:14 -0700

    handle cases where memory tools interpose library isn’t used

--- osrf_testing_tools_cpp/src/memory_tools/impl/unsupported_os.cpp
+++ osrf_testing_tools_cpp/src/memory_tools/impl/unsupported_os.cpp
@@ -12,12 +12,18 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include "osrf_testing_tools_cpp/memory_tools/visibility_control.hpp"
+
 namespace osrf_testing_tools_cpp
 {
 namespace memory_tools
 {
 
-// Nothing here for now.
+/// Function to ensure at least one symbol exists on Windows.
+OSRF_TESTING_TOOLS_CPP_MEMORY_TOOLS_EXPORT
+void
+do_not_use()
+{}
 
 }  // namespace memory_tools
 }  // namespace osrf_testing_tools_cpp
